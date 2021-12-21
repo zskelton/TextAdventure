edf7ac9840af49b330022e9d58f3602522c02559
Zachary D. Skelton <zbert@hotmail.com>
2021-12-21 15:51:29 -0600

    build(io): Using JSON for data management.

--- src/Game/Main.js
+++ src/Game/Main.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from "react";
 import "./Main.css";
 import { Hr } from "../Components/Hr";
+const gamedata = require("../Game/Levels/level1.json");
 
 export const Main = () => {
   // Variables
@@ -8,6 +9,62 @@ export const Main = () => {
   const [history, setHistory] = useState("You entered a forrest. You see a note.");
   const [lastCommand, setLastCommand] = useState("");
   const [inventory, setInventory] = useState([]);
+
+  console.log(gamedata);
+
+  /*
+
+    "Game": {
+    "Name": "Zack's Adventure",
+    "Size": [5, 5],
+    "Player": {
+      "Name": "Zack",
+      "HP": 30,
+      "Weapon": null,
+      "Gold": 0,
+      "Items": {}
+    },
+    "Rooms": [
+    {
+      "name": "Entryway",
+      "text_first": "You are in a forrest with a cave entrance directly to your east. You see a note on the ground.",
+      "text_long": "You are in a forrest with a cave entrance directly to your east.",
+      "text_short": "You are at the entryway of a cave.",
+      "x": 0,
+      "y": 0,
+      "directions": {
+        "north": false,
+        "east": true,
+        "south": false,
+        "west": false
+      },
+      "items": [
+        {
+          "name": "note",
+          "message": "You picked up a note saying 'Hi traveller!'"
+        }
+      ],
+      "enemies": [],
+      "isEnd": false
+    },
+
+  */
+
+
+  // Load Game
+  // - Set Name
+  // - Set Blank Map of Size
+  // - Display Player Info
+  // - Set Initial Text
+  // - Load Room
+  // - - Display Text
+  // - - Update Map
+  // - - Set Item/Enemy Variables
+  // - - Receive Command
+  // - - - Verb | Noun
+  // - - - Go [North, East, South, West], Attack [Enemy.Name], Get [Item.Name], Help <- Display This
+  // - - - Update JSON Text based on situation (enemy dies, note picked up etc.).
+  // - - - - Print Response to Command
 
   // Handlers
   const handleSubmit = () => {
@@ -18,6 +75,9 @@ export const Main = () => {
     command.current.focus();
   }
 
+
+  // Command Structure
+  // Verb Noun
   const onInput = command => {
     switch (command) {
       case "Go North":
@@ -57,7 +117,12 @@ export const Main = () => {
           />
         </section>
         <aside className="inventory">
-          Inventory: <br />
+          Player: {gamedata.Player.Name}<br />
+          Hit Points: {gamedata.Player.HP}<br />
+          Gold: {gamedata.Player.Gold}<br />
+          Weapon: {gamedata.Player.Weapon || "None"} <br />
+          <br />
+          Items: <br />
           {inventory.map((item) => {
             return(
               <li key={item}>{item}</li>
